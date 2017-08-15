67f92cce91d5988cbb882717fa1d2d47e8811ea3
Svend Küstermann <svend.kuestermann@gmail.com>
2017-08-15 10:55:33 +0200

    save for yesterday

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'wordgrid', to: 'game#wordgrid'
+
+  post 'score', to: 'game#score'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
